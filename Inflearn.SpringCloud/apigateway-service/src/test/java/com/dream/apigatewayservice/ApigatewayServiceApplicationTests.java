1ce6b3751cd65c433bcfbcba66f99fb6f0437270
link-mozy <rnjs213@gmail.com>
2022-06-30 18:57:29 +0900

    added spring api-gateway service

---
+++ Inflearn.SpringCloud/apigateway-service/src/test/java/com/dream/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dream.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
