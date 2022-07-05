699ddee511b2c66260a5109085eee045125e0148
link-mozy <rnjs213@gmail.com>
2022-07-05 11:36:10 +0900

    added load balancer

--- Inflearn.SpringCloud/first-service/src/main/java/com/dream/firstservice/FirstServiceController.java
+++ Inflearn.SpringCloud/first-service/src/main/java/com/dream/firstservice/FirstServiceController.java
@@ -1,10 +1,15 @@
 package com.dream.firstservice;
 
+import com.netflix.discovery.converters.Auto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 // http://localhost:8081/welcome
 // http://localhost:8081/first-service/welcome
@@ -12,6 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service")
 @Slf4j
 public class FirstServiceController {
+    Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
 
     @GetMapping("/welcome")
     public String welcome() {
@@ -25,7 +36,8 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+        return String.format("Hi, there. This is a message from First Service on PORT %s", env.getProperty("local.server.port"));
     }
 }
