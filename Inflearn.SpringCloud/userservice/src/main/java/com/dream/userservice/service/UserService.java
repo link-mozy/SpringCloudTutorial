b388725e33abc4d63d350ea59c47c5ea9e023dec
kwon.ohjeong <kwon.ohjeong@dreamsecurity.com>
2022-11-07 17:40:34 +0900

    updated userservice createUser method

---
+++ Inflearn.SpringCloud/userservice/src/main/java/com/dream/userservice/service/UserService.java
@@ -0,0 +1,7 @@
+package com.dream.userservice.service;
+
+import com.dream.userservice.dto.UserDto;
+
+public interface UserService {
+    UserDto createUser(UserDto userDto);
+}
---
+++ Inflearn.SpringCloud/userservice/src/main/java/com/dream/userservice/service/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.dream.userservice.service;
+
+import com.dream.userservice.dto.UserDto;
+import com.dream.userservice.jpa.UserEntity;
+import com.dream.userservice.jpa.UserRepository;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+public class UserServiceImpl implements UserService{
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDto createUser(UserDto userDto) {
+        userDto.setUserId(UUID.randomUUID().toString());
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+        UserEntity userEntity = mapper.map(userDto, UserEntity.class);
+        userEntity.setEncryptedPwd("encrypted_password");
+
+        userRepository.save(userEntity);
+
+        UserDto resultUserDto = mapper.map(userEntity, UserDto.class);
+
+        return resultUserDto;
+    }
+}
